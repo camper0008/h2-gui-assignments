b3d7ae4087a5b222882df11c87611143799347c5
Theis Pieter Hollebeek <tphollebeek@gmail.com>
2023-03-08 15:05:50 +0100

    unqualify egui use, use imports instead

--- 00-color-picker/src/main.rs
+++ 00-color-picker/src/main.rs
@@ -1,7 +1,8 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
-use eframe::egui;
-use egui::{ColorImage, TextureHandle, TextureOptions};
+use egui::{
+    vec2, Button, CentralPanel, ColorImage, Context, Slider, TextureHandle, TextureOptions,
+};
 
 fn main() -> Result<(), eframe::Error> {
     let options = eframe::NativeOptions {
@@ -50,19 +51,19 @@ impl ColorPickerApp {
 }
 
 impl eframe::App for ColorPickerApp {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
+    fn update(&mut self, ctx: &Context, _frame: &mut eframe::Frame) {
+        CentralPanel::default().show(ctx, |ui| {
             ctx.set_pixels_per_point(1.5);
             let texture: &TextureHandle = {
                 let color_image = ColorImage::from_rgb([1, 1], &[self.r, self.g, self.b]);
                 let texture_options = TextureOptions::default();
                 &ui.ctx().load_texture("color", color_image, texture_options)
             };
-            ui.image(texture, egui::vec2(200.0, 100.0));
+            ui.image(texture, vec2(200.0, 100.0));
             ui.add_space(10.0);
-            ui.add(egui::Slider::new(&mut self.r, 0..=255).text("red"));
-            ui.add(egui::Slider::new(&mut self.g, 0..=255).text("green"));
-            ui.add(egui::Slider::new(&mut self.b, 0..=255).text("blue"));
+            ui.add(Slider::new(&mut self.r, 0..=255).text("red"));
+            ui.add(Slider::new(&mut self.g, 0..=255).text("green"));
+            ui.add(Slider::new(&mut self.b, 0..=255).text("blue"));
             ui.add_space(10.0);
             ui.horizontal(|ui| {
                 ui.style_mut().spacing.text_edit_width = 55.0;
@@ -70,7 +71,7 @@ impl eframe::App for ColorPickerApp {
                 ui.text_edit_singleline(&mut self.hex)
                     .labelled_by(hex_label.id);
                 if ui
-                    .add_enabled(self.valid_hex(), egui::Button::new("convert hex to rgb"))
+                    .add_enabled(self.valid_hex(), Button::new("convert hex to rgb"))
                     .clicked()
                 {
                     self.hex_to_rgb();
